75a7220b4830dc0c7781887d399f89851dca6c81
Luiz Henrique <luizhjramos@outlook.com>
2022-02-15 19:55:03 -0300

    chore: add react-query

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -74,29 +74,27 @@ const Home = (): JSX.Element => {
   // Render
   // -------------------------------------------------
   return (
-    <>
-      <ProductList>
-        {products.map((product) => (
-          <li>
-            <img src={product.image} alt={product.title} />
-            <strong>{product.title}</strong>
-            <span>{product.priceFormatted}</span>
-            <button
-              type="button"
-              data-testid="add-product-button"
-              // onClick={() => handleAddProduct(product.id)}
-            >
-              <div data-testid="cart-product-quantity">
-                <MdAddShoppingCart size={16} color="#FFF" />
-                {/* {cartItemsAmount[product.id] || 0} */} 2
-              </div>
+    <ProductList>
+      {products.map((product) => (
+        <li>
+          <img src={product.image} alt={product.title} />
+          <strong>{product.title}</strong>
+          <span>{product.priceFormatted}</span>
+          <button
+            type="button"
+            data-testid="add-product-button"
+            // onClick={() => handleAddProduct(product.id)}
+          >
+            <div data-testid="cart-product-quantity">
+              <MdAddShoppingCart size={16} color="#FFF" />
+              {/* {cartItemsAmount[product.id] || 0} */} 2
+            </div>
 
-              <span>ADICIONAR AO CARRINHO</span>
-            </button>
-          </li>
-        ))}
-      </ProductList>
-    </>
+            <span>ADICIONAR AO CARRINHO</span>
+          </button>
+        </li>
+      ))}
+    </ProductList>
   );
 };
 
